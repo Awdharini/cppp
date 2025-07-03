5d572eb6b51d034472f5a1ae95477f43212f2608
Awdharini <190753477+Awdharini@users.noreply.github.com>
2025-07-03 18:56:04 +0530

    Add files via upload

---
+++ c++ infosys springboard/6-functions/ufo_functions.hpp
@@ -0,0 +1,8 @@
+#include <vector>
+
+void display_misses(int misses);
+
+// Declare functions
+void greet();
+void display_status(std::vector<char> incorrect,char answer);
+void end_game(char answer,char codeword);
---
+++ c++ infosys springboard/8-references-and-pointers/functions.hpp
@@ -0,0 +1,3 @@
+void bleep(char word,char &text);
+void asterisk(char word, char &text, int i);
+void bleep(char word,char sentence);
